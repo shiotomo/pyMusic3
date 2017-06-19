82439dadd831e39b4f52996c6fe8e9472a62a3ac
shiotomo <t.shiota16@gmail.com>
2017-06-20 01:46:13 +0900

    Implement music start !!

--- musicStart.py
+++ musicStart.py
@@ -1,13 +1,32 @@
-from readJson import *
+from readJson import ReadJson
+from path import Path
 
 import os
-import json
+import pygame
+import time
+from mutagen.mp3 import MP3
 
 class MusicStart:
     def single(self, number):
         readJson = ReadJson()
+        path = Path()
+        pwd = path.path()
+
         musicData = readJson.JsonSelector(number)
         print(musicData)
+
+        audio = pwd + musicData
+
+        audioLen = MP3(audio)
+        pygame.mixer.init()
+        pygame.mixer.music.load(audio)
+        pygame.mixer.music.play(1)
+
+        print("---"+ musicData +"---")
+
+        time.sleep(audioLen.info.length)
+        pygame.mixer.music.stop()
+
 
     def loop(self, number):
         pass
